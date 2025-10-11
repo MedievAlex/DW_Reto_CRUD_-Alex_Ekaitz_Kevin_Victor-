d57966d14b3f18d0569cf258dde60e4edfa13288
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2025-10-11 12:03:48 +0200

    Creating Php Files

---
+++ Model/Profile.php
@@ -0,0 +1,51 @@
+<?php
+abstract class Profile
+{
+  protected $email;
+  protected $username;
+  protected $password;
+  protected $name;
+  protected $lastname;
+  protected $telephone;
+
+  public function __construct($email, $username, $name, $password, $lastname, $telephone)
+  {
+    $this->email = $email;
+    $this->username = $username;
+    $this->password = $password;
+    $this->name = $name;
+    $this->lastname = $lastname;
+    $this->telephone = $telephone;
+  }
+
+  public function getEmail()
+  {
+    return $this->email;
+  }
+  public function getUsername()
+  {
+    return $this->username;
+  }
+  public function getPassword()
+  {
+    return $this->password;
+  }
+  public function setPassword($password)
+  {
+    $this->password = $password;
+  }
+  public function getName()
+  {
+    return $this->name;
+  }
+  public function getLastname()
+  {
+    return $this->lastname;
+  }
+  public function getTelephone()
+  {
+    return $this->telephone;
+  }
+
+  abstract public function mostrar();
+}
---
+++ Model/User.php
@@ -0,0 +1,28 @@
+<?php
+class User extends Profile
+{
+  private $gender;
+  private $cardNumber;
+
+  public function __construct($email, $username, $name, $password, $lastname, $telephone, $gender, $cardNumber)
+  {
+    parent::__construct($email, $username, $name, $password, $lastname, $telephone);
+    $this->gender = $gender;
+    $this->cardNumber = $cardNumber;
+  }
+
+  public function getGender()
+  {
+    return $this->gender;
+  }
+
+  public function getCardNumber()
+  {
+    return $this->cardNumber;
+  }
+
+  public function mostrar()
+  {
+    return "[USER - " . parent::mostrar() . " - Gender: $this->gender - Card Number: $this->cardNumber]";
+  }
+}
