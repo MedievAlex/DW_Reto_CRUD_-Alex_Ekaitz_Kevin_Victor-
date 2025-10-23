232e25069e21fc2a6cf70c0bc9fd5f1a71a05c75
MedievAlex <alexirazola@gmail.com>
2025-10-23 12:28:30 +0200

    No entiendo na de lo que estoy haciendo con php.

---
+++ JS/login.js
@@ -0,0 +1,13 @@
+const credential = document.getElementById('credential');
+const passwd = document.getElementById('password');
+const loginBtn = document.getElementById('loginBtn');
+
+loginBtn.addEventListener("click", () => {
+    verifyUser()
+});
+
+function verifyUser() {
+    const response = fetch('../Api/login.php');
+
+
+}
