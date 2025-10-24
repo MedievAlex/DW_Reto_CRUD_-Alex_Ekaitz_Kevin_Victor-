0bcfcdf0409228dd33d0faa7f8be6c2825806551
Ekaitz Campo <116925888+Ekasestao@users.noreply.github.com>
2025-10-24 23:06:26 +0200

    Changing quotes on sql

--- Sql/BD_Reto_CRUD_(Alex_Ekaitz_Kevin_Victor).sql
+++ Sql/BD_Reto_CRUD_(Alex_Ekaitz_Kevin_Victor).sql
@@ -31,13 +31,13 @@ CREATE TABLE db_admin (
 
 -- INSERTS IN THE TABLES --
 INSERT INTO db_profile (P_EMAIL, P_USERNAME, P_PASSWORD, P_NAME, P_LASTNAME, P_TELEPHONE) VALUES
-('admin@admin.com', 'admin', '1234', "Admin", "Admin", 123456789);
+('admin@admin.com', 'admin', '1234', 'Admin', 'Admin', 123456789);
 
 INSERT INTO db_profile (P_EMAIL, P_USERNAME, P_PASSWORD, P_NAME, P_LASTNAME, P_TELEPHONE) VALUES
-('user@user.com', 'user', '1234', "User", "User", 987654321);
+('user@user.com', 'user', '1234', 'User', 'User', 987654321);
 
 INSERT INTO db_admin (A_ID, A_CURRENT_ACCOUNT) VALUES
-(1, "1234123412341234");
+(1, '1234123412341234');
 
 INSERT INTO db_user (U_ID, U_GENDER, U_CARD) VALUES
-(2, 'MALE', "4321432143214321");
+(2, 'MALE', '4321432143214321');
