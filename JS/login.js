e19f772cf40fc4a9f9475f2b037f91d02b6a21c8
MedievAlex <alexirazola@gmail.com>
2025-10-24 09:47:24 +0200

    Avances!

--- JS/login.js
+++ JS/login.js
@@ -2,20 +2,33 @@ const credential = document.getElementById('credential');
 const passwd = document.getElementById('password');
 const loginBtn = document.getElementById('loginBtn');
 
-loginBtn.addEventListener("click", () => {
-    credential = document.getElementById('credential');
-    passwd = document.getElementById('password');
-    credential = document.getElementById('credential');
-    verifyUser(credential, passwd);
+loginBtn.addEventListener("click", () => 
+{
+    credential = document.getElementById('credential').value;
+    passwd = document.getElementById('password').value;
+    credential = document.getElementById('credential').value;
+    
+    if(document.getElementById('credential').value == "" || document.getElementById('password').value == "")
+    {
+        alert("Los campos no pueden estar vacios.");
+    }
+    else
+    {
+        verifyUser(credential, passwd);
+    }
 });
 
-async function verifyUser(credential, passwd) {
+async function verifyUser(credential, passwd) 
+{
     const response = await fetch(`../Api/login.php`);
     const user = response.json();
 
-    if (user.error) {
+    if (user.error) 
+    {
         alert("Error fetching user: " + user.error);
-    } else{
+    } 
+    else
+    {
         userData = user;
     }
 }
