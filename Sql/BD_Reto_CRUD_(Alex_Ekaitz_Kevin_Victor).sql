dc964888a19ca602e217796ca090320332bdddf9
MedievAlex <alexirazola@gmail.com>
2025-10-28 09:40:16 +0100

    Updated the DB to have more users to test.

--- Sql/BD_Reto_CRUD_(Alex_Ekaitz_Kevin_Victor).sql
+++ Sql/BD_Reto_CRUD_(Alex_Ekaitz_Kevin_Victor).sql
@@ -32,10 +32,14 @@ CREATE TABLE db_admin (
 -- INSERTS IN THE TABLES --
 INSERT INTO db_profile (P_EMAIL, P_USERNAME, P_PASSWORD, P_NAME, P_LASTNAME, P_TELEPHONE) VALUES
 ('admin@admin.com', 'admin', '1234', 'Admin', 'Admin', '123456789'),
-('user@user.com', 'user', '1234', 'User', 'User', '987654321');
+('user1@user.com', 'user1', '1234', 'User', 'User', '987654321'),
+('user2@user.com', 'user2', '1234', 'User', 'User', '987867321'),
+('user3@user.com', 'user3', '1234', 'User', 'User', '987864451');
 
 INSERT INTO db_admin (A_ID, A_CURRENT_ACCOUNT) VALUES
 (1, '1234123412341234');
 
 INSERT INTO db_user (U_ID, U_GENDER, U_CARD) VALUES
-(2, 'MALE', '4321432143214321');
+(2, 'MALE', '4321432143214321'),
+(3, 'FEMALE', '4321432337914321'),
+(4, 'OTHER', '4305332143214321');
