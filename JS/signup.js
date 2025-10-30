9f6a4645763f68c83e9d8f2bedf551f9f864c0c8
MedievAlex <alexirazola@gmail.com>
2025-10-30 12:46:51 +0100

    Added password pattern.

--- JS/signup.js
+++ JS/signup.js
@@ -1,27 +1,34 @@
 async function createUser(event) {
   event.preventDefault();
 
-  const form = document.getElementById("signup-form");
-  const formData = new FormData(form);
+  pattern = /^(?=.*[0-9])(?=.*[A-Za-z]).{8,}$/;
+	
+  if (pattern.test(document.getElementById("password").value)) {
+    const form = document.getElementById("signup-form");
+    const formData = new FormData(form);
 
-  try {
-    const response = await fetch(`../Api/User.php`, {
-      method: "POST",
-      body: formData,
-    });
+    try {
+      const response = await fetch(`../Api/User.php`, {
+        method: "POST",
+        body: formData,
+      });
 
-    const result = await response.json();
+      const result = await response.json();
 
-    if (result.success) {
-      localStorage.setItem("type", "user");
-      localStorage.setItem("id", result.user.id);
-      localStorage.setItem("username", result.user.username);
-      window.location.href = "user.html";
-    } else {
-      alert("Error: " + result.message);
+      if (result.success) {
+        localStorage.setItem("type", "user");
+        localStorage.setItem("id", result.user.id);
+        localStorage.setItem("username", result.user.username);
+        window.location.href = "user.html";
+      } else {
+        alert("Error: " + result.message);
+      }
+    } catch (error) {
+      alert("An error occurred while creating the user.");
     }
-  } catch (error) {
-    alert("An error occurred while creating the user.");
+  }
+  else {
+    alert("Password must have at least 8 characters and contain one letter and one number.")
   }
 }
 
